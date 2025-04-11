08a15eabbfaad04dab7b30eef52dde8b79c612fc
Hamidov Siyovush <161587226+siyovush-hamidov@users.noreply.github.com>
2025-04-11 08:11:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,9 @@ def resize_image(img_path, max_width=400):
     return img
 
 def predict(name):
-    """Функция для загрузки и предсказания по изображению."""
-    image = st.file_uploader("Загрузите фотографию " + name, type=["png", "jpg", "jpeg"])
+    """Функция загрузки и предсказания по изображению."""
+    image = st.file_uploader("Аксро бор кунед: " + name, type=["png", "jpg", "jpeg"])
     if image:
-        st.image(image=image)
         im = Image.open(image)
         SamplePhoto = np.asarray(im)
         resized_arr = cv2.resize(SamplePhoto, (img_size, img_size))
@@ -42,33 +41,36 @@ def predict(name):
             ANS = str("%.2f" % FloatNumber)
 
             if FloatNumber > 60:
-                st.markdown("<h4 style='text-align: center; color: white;'>Обнаружены признаки пневмонии.</h4>", unsafe_allow_html=True)
-                st.markdown(f"<h5 style='text-align: center; color: white;'>Вероятность наличия составляет {ANS}%</h5>", unsafe_allow_html=True)
+                st.markdown("<h4 style='text-align: center; color: red;'>Аломатҳои бемории илтиҳоби шуш мушоҳида мешаванд.</h4>", unsafe_allow_html=True)
+                st.markdown(f"<h5 style='text-align: center; color: red;'>Эҳтимолияти беморӣ: {ANS}%</h5>", unsafe_allow_html=True)
             else:
-                st.markdown("<h4 style='text-align: center; color: white;'>Признаков заболевания не обнаружено.</h4>", unsafe_allow_html=True)
-                st.markdown(f"<h5 style='text-align: center; color: white;'>Вероятность наличия составляет {ANS}%</h5>", unsafe_allow_html=True)
+                st.markdown("<h4 style='text-align: center; color: green;'>Аломатҳои беморӣ мушоҳида намешаванд.</h4>", unsafe_allow_html=True)
+                st.markdown(f"<h5 style='text-align: center; color: green;'>Эҳтимолияти беморӣ: {ANS}%</h5>", unsafe_allow_html=True)
+
+            st.image(image=image, caption='Аксҳои рентгении таҳлилшуда', use_column_width=True)
+
         except Exception as e:
-            st.error(f"Ошибка при работе с моделью: {str(e)}")
+            st.error(f"Хатогӣ ҳангоми истифодаи модел: {str(e)}")
 
 def main():
-    """Главная функция для отображения страницы Streamlit."""
-    st.markdown("<h2 style='text-align: center; color: white;'>Модель машинного обучения для диагностирования бактериальной и вирусной пневмонии</h2>", unsafe_allow_html=True)
+    """Саҳифаи асосии Streamlit."""
+    st.markdown("<h2 style='text-align: center; color: white;'>Модели омӯзиши мошинӣ барои ташхиси илтиҳоби шуш</h2>", unsafe_allow_html=True)
     st.image(
         'miscellaneous/attention.webp',
-        caption='“Здоровье перевешивает все остальные блага жизни. —Артур Шопенгауэр”',
+        caption='“Саломатӣ беҳтар аз ҳама неъматҳост. —Артур Шопенгауэр”',
         use_column_width=True
     )
-    st.markdown("<h3 style='text-align: center; color: white;'>Как работает модель?</h3>", unsafe_allow_html=True)
-    st.write("Пользователь должен загрузить рентгеновский снимок, чтобы получить результат в виде вероятности наличия заболевания.")
-    
-    # Отображение изображений для примера
-    col1, col2 = st.columns(2)
-    col1.image(resize_image('xray_samples/Healthy.jpg'), caption='Снимок грудной клетки здорового пациента')
-    col2.image(resize_image('xray_samples/Pneumonia.jpeg'), caption='Снимок грудной клетки, пораженной болезнью')
+    st.markdown("<h3 style='text-align: center; color: white;'>Модел чӣ гуна кор мекунад?</h3>", unsafe_allow_html=True)
+    st.write("Истифодабаранда бояд акси рентгениро бор кунад ва модел натиҷаи эҳтимолияти бемориро нишон медиҳад.")
 
-    # Загрузка и предсказание
-    st.markdown("<h5 style='text-align: center; color: white;'>Загрузите снимок</h5>", unsafe_allow_html=True)
-    predict('image')  # Передаем имя загруженного файла
+    # Намоиши мисолҳо
+    col1, col2 = st.columns(2)
+    col1.image(resize_image('xray_samples/Healthy.jpg'), caption='Синаи шахси солим')
+    col2.image(resize_image('xray_samples/Pneumonia.jpeg'), caption='Синаи шахси бемор')
+
+    # Боргузорӣ ва пешгӯӣ
+    st.markdown("<h5 style='text-align: center; color: white;'>Аксро бор кунед</h5>", unsafe_allow_html=True)
+    predict('акс')  # Номи оддии тасвир барои боркунӣ
 
 if __name__ == "__main__":
     main()
